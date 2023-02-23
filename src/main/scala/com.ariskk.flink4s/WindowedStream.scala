26b32394df1d270a4ace02a63d7e12488f838427
Wosin <blind.van@gmail.com>
2023-02-23 18:42:24 +0100

    Fix aggregate + remove monoid.

--- src/main/scala/com.ariskk.flink4s/WindowedStream.scala
+++ src/main/scala/com.ariskk.flink4s/WindowedStream.scala
@@ -18,21 +18,22 @@ final case class WindowedStream[T, K, W <: Window](stream: JavaWStream[T, K, W])
     DataStream(stream.reduce(reducer))
   }
 
-  def aggregate[A, O](aggregateF: (A, T) => A)(outputF: A => O)(implicit monoid: Monoid[A],
-                                                                aggTypeInformation: TypeInformation[A],
-                                                                typeInformation: TypeInformation[O]): DataStream[O] = {
+  def aggregate[A, O](agg: A, mergeF: (A, A) => A)(aggregateF: (A, T) => A)(outputF: A => O)(
+      implicit
+      aggTypeInformation: TypeInformation[A],
+      typeInformation: TypeInformation[O]
+  ): DataStream[O] = {
     val reducer = new AggregateFunction[T, A, O] {
 
-      override def createAccumulator(): A = Monoid.empty[A]
+      override def createAccumulator(): A = agg
 
       override def add(value: T, accumulator: A): A = aggregateF(accumulator, value)
 
       override def getResult(accumulator: A): O = outputF(accumulator)
 
-      override def merge(a: A, b: A): A = Monoid
-        .combine(a, b)
+      override def merge(a: A, b: A): A = mergeF(a, b)
     }
-    DataStream(stream.aggregate[A,O](reducer, aggTypeInformation, typeInformation))
+    DataStream(stream.aggregate[A, O](reducer, aggTypeInformation, typeInformation))
   }
 
 }
